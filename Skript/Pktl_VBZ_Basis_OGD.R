0e663280dd5a42eee3d653fa8e0930b2a9c82d6b
Your <christoph.baur@vbz.ch>
2021-01-06 23:05:21 +0100

    Einfügen diverser Berechnungen

--- Skript/Pktl_VBZ_Basis_OGD.R
+++ Skript/Pktl_VBZ_Basis_OGD.R
@@ -25,10 +25,11 @@ gc()
 library(dplyr)
 library(tidyr)
 library(readr)
+library(lubridate)
 
 
-# Input-path (where the ogd-data is to find on your disk)
-input_path <- ("01_Data/01_Input/")
+# Input-path 
+input_path <- ("C:/Users/Chris/Documents/R/VBZ_Pktl_vergleich/Daten/Input/")
 
 
 #_________________________________________________________________
@@ -48,100 +49,106 @@ haltestelle <- read_csv(paste0(input_path,"haltestelle.csv"))
 
 #Main table
 
-#how many "fahrzeiten*" files exist in your folder?
-#be carefull, one "fahrzeiten"-dataset is about 250 MB
-#make sure your working-directory is set up correct so R can find your files
+#how many "fahrzeiten*" files exists in folder?
 file_list <- list.files(input_path,
                         pattern = "fahrzeiten")
 
+#for each file tmp loading, extract the needed information, remove tmp
+#Hintergrund: begrenzter RAM
 for (file in file_list){
   # if the merged dataset does exist, append to it
   if (exists("fahrzeiten")){
+    #temporär Daten laden
     temp_dataset <- read_csv(paste0(input_path,file))
+    
+    #weitere Berechnungen
+    temp_dataset <- temp_dataset%>%
+      #Jahr/Kalenderwoche berechnen
+      mutate(jahr=year(as.Date(betriebsdatum, format="%d.%m.%y")),
+             kw=isoweek(as.Date(betriebsdatum, format="%d.%m.%y")))%>%
+      #Ankunftsverspätung errechnen
+      mutate(gap_arr=(ist_an_nach1-soll_an_nach))%>%
+      #kategorisieren
+      mutate(punct_cat = case_when(gap_arr<0 ~ "lower_0min",
+                                   gap_arr>=0 & gap_arr <60 ~ "0_1min",
+                                   gap_arr>=60 & gap_arr <120 ~ "1_2min",
+                                   gap_arr>=120 & gap_arr <180 ~ "2_3min",
+                                   gap_arr>=180 & gap_arr <240 ~ "3_4min",
+                                   gap_arr>=240 & gap_arr <300 ~ "4_5min",
+                                   gap_arr>=300 & gap_arr <360 ~ "5_6min",
+                                   gap_arr>=360  ~ "higher_6min"))%>%
+      #aggregieren
+      group_by(jahr, kw, punct_cat)%>%
+      summarise(anzahl=n())%>%
+      ungroup()
+    
+    #hinzufügen zu existierendem Datensatz
     fahrzeiten<-rbind(fahrzeiten, temp_dataset)
+    #temporäre Daten löschen
     rm(temp_dataset)
   }
   
   # if the merged dataset doesn't exist, create it
   if (!exists("fahrzeiten")){
+    #Daten laden
     fahrzeiten <- read_csv(paste0(input_path,file))
+    
+    #weitere Berechnungen
+    fahrzeiten <- fahrzeiten%>%
+      #Jahr/Kalenderwoche berechnen
+      mutate(jahr=year(as.Date(betriebsdatum, format="%d.%m.%y")),
+             kw=isoweek(as.Date(betriebsdatum, format="%d.%m.%y")))%>%
+      #Ankunftsverspätung errechnen
+      mutate(gap_arr=(ist_an_nach1-soll_an_nach))%>%
+      #kategorisieren
+      mutate(punct_cat = case_when(gap_arr<0 ~ "lower_0min",
+                                   gap_arr>=0 & gap_arr <60 ~ "0_1min",
+                                   gap_arr>=60 & gap_arr <120 ~ "1_2min",
+                                   gap_arr>=120 & gap_arr <180 ~ "2_3min",
+                                   gap_arr>=180 & gap_arr <240 ~ "3_4min",
+                                   gap_arr>=240 & gap_arr <300 ~ "4_5min",
+                                   gap_arr>=300 & gap_arr <360 ~ "5_6min",
+                                   gap_arr>=360  ~ "higher_6min"))%>%
+      #aggregieren
+      group_by(jahr, kw, punct_cat)%>%
+      summarise(anzahl=n())%>%
+      ungroup()
+               
   }
   
 }
 
-
-#### >Some examples ####
-#### >> Matching ####
-fahrzeiten_full <- fahrzeiten%>%
-  #join haltepunkt to "from" ("von")
-  left_join(haltepunkt,
-            by=c("halt_punkt_id_von"="halt_punkt_id",
-                 "halt_punkt_diva_von"="halt_punkt_diva",
-                 "halt_id_von"="halt_id"))%>%
-  #rename new columns it with suffix "von"
-  rename(GPS_Latitude_von=GPS_Latitude,
-         GPS_Longitude_von=GPS_Longitude,
-         GPS_Bearing_von=GPS_Bearing,
-         halt_punkt_ist_aktiv_von=halt_punkt_ist_aktiv)%>%
-  #join haltepunkt to "to" ("nach")
-  left_join(haltepunkt,
-            by=c("halt_punkt_id_nach"="halt_punkt_id",
-                 "halt_punkt_diva_nach"="halt_punkt_diva",
-                 "halt_id_nach"="halt_id"))%>%
-  #rename new columns it with suffix "nach"
-  rename(GPS_Latitude_nach=GPS_Latitude,
-         GPS_Longitude_nach=GPS_Longitude,
-         GPS_Bearing_nach=GPS_Bearing,
-         halt_punkt_ist_aktiv_nach=halt_punkt_ist_aktiv)%>%
-  #join haltestelle to "from" ("von")
-  left_join(haltestelle,
-            by=c("halt_id_von"="halt_id",
-                 "halt_diva_von"="halt_diva",
-                 "halt_kurz_von1"="halt_kurz"))%>%
-  #rename new columns it with suffix "von"
-  rename(halt_lang_von=halt_lang,
-         halt_ist_aktiv_von=halt_ist_aktiv)%>%
-  #join haltestelle to "to" ("nach")
-  left_join(haltestelle,
-            by=c("halt_id_nach"="halt_id",
-                 "halt_diva_nach"="halt_diva",
-                 "halt_kurz_nach1"="halt_kurz"))%>%
-  #rename new columns it with suffix "nach"
-  rename(halt_lang_nach=halt_lang,
-         halt_ist_aktiv_nach=halt_ist_aktiv)
-
-
-#### >> Punctuality per line ####
-# whats punctuality? 
-# VBZ define an belated arrival lower than 2min as "on time"
-# also the departure must not be more than 1min before the scheduled time 
-# < -60s "not on time/too early"
-# <= -60s ... <= 120s "on time"
-# > 120s "not on time/delayed"
-
-punctuality <- fahrzeiten_full%>%
-  #Calculate gap between scheduled and acuatal arrival/departuretimes
-  # at "nach" stop
-  mutate(gap_arr=(ist_an_nach1-soll_an_nach),
-         gap_dep=(ist_ab_nach-soll_ab_nach))%>%
-  #Calculate punctual category
-  mutate(punct_cat = case_when(gap_arr>=120 ~ "delayed",
-                               gap_dep<= -60 ~ "to early",
-                               TRUE~ "punctual"))%>%
-  #group per line and punctual category
-  group_by(linie, punct_cat)%>%
+#### Pünktlichkeit im Netz, gesamter Zeitraum #### 
+pktl_gesamt <- fahrzeiten%>%
+  #group per punctual category
+  group_by(punct_cat)%>%
   #sum it up
-  summarise(count=n())%>%
+  summarise(anzahl=sum(anzahl))%>%
   #and create percentage value
-  mutate(percentage=100*(count/sum(count)))
+  mutate(percentage=100*(anzahl/sum(anzahl)))
 
+#gemäss vereinfachter Definition VBZ
+#ab 2 min verspätet
+#(ohne verfrühte Abfahrten als unpünktlich zu zählen)
+pktl_vbz <- pktl_gesamt%>%
+  filter(punct_cat=="lower_0min" | 
+           punct_cat=="0_1min" |
+           punct_cat=="1_2min")%>%
+  summarise(prozent=sum(percentage))
 
-#may you want to spread the punct_cat?
-punctuality_spread <- punctuality%>%
-  #select relevant columns
-  select(linie,
-         punct_cat,
-         percentage)%>%
-  #use spread
-  spread(key=punct_cat,
-         value=percentage)
+  
+# gemäss Definition Deutsche Bahn
+#ab 6 min
+pktl_db <- pktl_gesamt%>%
+  filter(!punct_cat=="higher_6min")%>%
+  summarise(prozent=sum(percentage))
+  
+# gemäss Definition SBB
+#ab 3 min
+pktl_sbb <- pktl_gesamt%>%
+  filter(punct_cat=="lower_0min" | 
+           punct_cat=="0_1min" |
+           punct_cat=="1_2min" |
+           punct_cat=="2_3min")%>%
+  summarise(prozent=sum(percentage))
+
